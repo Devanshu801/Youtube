b9b0646461eb714e01e88aaac74dc059a5de40a1
devanshu801 <devanshubansal14@gmail.com>
2021-02-11 23:35:26 +0530

    new project

---
+++ src/main/java/com/craterzone/demo/controller/UserController.java
@@ -0,0 +1,74 @@
+package com.craterzone.demo.controller;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.craterzone.demo.model.Address;
+import com.craterzone.demo.model.User;
+import com.craterzone.demo.service.UserService;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+
+	@GetMapping("/")
+	private ResponseEntity<Optional<List<User>>> getAll(){
+		Optional<List<User>> userid = userService.getAll();
+		if(userid!=null) {
+			return ResponseEntity.status(HttpStatus.OK).body(userid);
+	}
+			return ResponseEntity.noContent().build();
+	 }
+	
+	@DeleteMapping("{id}")
+	private ResponseEntity<User> deleteUser(@PathVariable("id") int id){
+		userService.deleteById(id);
+		if(!Objects.isNull(id)) {
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(null);
+	}
+		return new ResponseEntity<User>(HttpStatus.BAD_REQUEST);
+	}
+	
+	@PostMapping("")
+	private ResponseEntity<User> registerUser(@RequestBody User user){
+		
+		Optional<User> userd = userService.registerUser(user);
+		if(!Objects.isNull(userd)) {
+			return ResponseEntity.status(HttpStatus.CREATED).body(user);
+		}
+		return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body(user);
+	}
+	@PostMapping("/log")
+	private ResponseEntity<Optional<User>> login(@RequestBody User user){
+		Optional<User> userdb = userService.login(user);
+		if(userdb!=null) {
+			return ResponseEntity.status(HttpStatus.ACCEPTED).body(userdb);
+		}
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+	}
+	@PatchMapping("{id}/fetch")
+	private ResponseEntity updateAddress(@PathVariable("id") int id,@RequestBody Address address) {
+		Optional<User> addres = userService.updateAddress(id, address);
+		if(addres.isPresent()) {
+			return ResponseEntity.status(HttpStatus.OK).body(addres);
+		}
+		return ResponseEntity.badRequest().build();
+	}
+}
---
+++ src/main/java/com/craterzone/demo/model/User.java
@@ -0,0 +1,98 @@
+package com.craterzone.demo.model;
+
+
+import javax.persistence.Id;
+
+
+
+public class User {
+
+	
+	@Id
+	private int id;
+	private  String firstName;
+	private String lastName;
+	private long dateOfBirth;
+	private String email;
+ 	private  Address address;
+	private  long created;
+	private  long updated;
+	private  String emailPassword;
+	
+	
+	
+	public User() {
+		super();
+	
+	}
+	public User(int id, String firstName, String lastName, long dateOfBirth, String email, Address address,
+			long created, long updated, String emailPassword) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.dateOfBirth = dateOfBirth;
+		this.email = email;
+		this.address = address;
+		this.created = created;
+		this.updated = updated;
+		this.emailPassword = emailPassword;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public long getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(long dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public long getCreated() {
+		return created;
+	}
+	public void setCreated(long created) {
+		this.created = created;
+	}
+	public long getUpdated() {
+		return updated;
+	}
+	public void setUpdated(long updated) {
+		this.updated = updated;
+	}
+	public String getEmailPassword() {
+		return emailPassword;
+	}
+	public void setEmailPassword(String emailPassword) {
+		this.emailPassword = emailPassword;
+	}
+	
+	
+	
+}
